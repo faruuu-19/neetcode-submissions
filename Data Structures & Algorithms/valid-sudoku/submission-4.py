a8946130d23a78568899b0f272db1739ed075fa4
Faryal <faryalnet34@gmail.com>
2026-07-21 21:07:58 +0500

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,51 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i , row in enumerate(board):
+            freq={}
+            for num in row:
+                if num in freq:
+                    freq[num]+=1
+                else:
+                    freq[num]=1
+                if freq[num]>1 and num!='.':
+                    print("hi1")
+                    return False
+            print(freq)
+        boardT=[]
+        for i in range(len(board[0])):
+            col = [row[i] for row in board]
+            boardT.append(col)
+        for i , row in enumerate(boardT):
+            freq={}
+            for num in row:
+                if num in freq:
+                    freq[num]+=1
+                else:
+                    freq[num]=1
+                if freq[num]>1 and num!='.':
+                    print("hi2")
+                    return False
+            print(freq)
+        subgrids = {}
+        k=0
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                temp = [row[j:j+3] for row in board[i:i+3]]
+                subgrids[k]=temp
+                k+=1
+        for key in subgrids:
+            flat=[]
+            for subarr in subgrids[key]:
+                flat.extend(subarr)
+            while '.' in flat:
+                flat.remove('.')
+            flatset=set(flat)
+            print(flat)
+            if len(flatset)!= len (flat):
+                return False
+
+        return True
+
+
+
+        
